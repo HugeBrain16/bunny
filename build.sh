9770f8db23c6d5bb2830c8e5b3cbdc0cd8fad219
HugeBrain16 <hugebrain16@gmail.com>
2026-04-12 20:00:44 +0700

    test

--- build.sh
+++ build.sh
@@ -34,7 +34,9 @@ pyinstaller --onedir --name "$APP_NAME" \
     --add-data "static:static" \
     --hidden-import webview \
     --hidden-import webview.platforms.qt \
-    --hidden-import qtpy \
+    --exclude-module PyQt6 \
+    --exclude-module PySide6 \
+    --exclude-module qtpy \
     --clean \
     --noconfirm \
     web.py
@@ -67,6 +69,6 @@ if [[ -f "static/icon.png" ]]; then
     cp "static/icon.png" "$ICON_DST"
 fi
 
-ARCH=$ARCH "$APPIMAGETOOL" "$APPDIR" "$OUTPUT"
+ARCH=$ARCH APPIMAGE_EXTRACT_AND_RUN=1 "$APPIMAGETOOL" "$APPDIR" "$OUTPUT"
 
 bash ./clean.sh
